44f3b5d2ef15b96883334bc576f7f0f685323b15
Tam Trieu Tri <trieutritam@gmail.com>
2024-05-11 04:39:15 +0000

    - Fix scikit-learn incorrect version. - Fix lint error

--- app.py
+++ app.py
@@ -32,11 +32,8 @@ def predict():
         # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
         # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
         clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
-    except FileExistsError:
+    except:
         LOG.info("JSON payload: %s json_payload")
-        return "Model not loaded"
-    else:
-        LOG.info("Load joblib error")
         return "Model not loaded"
         
 
